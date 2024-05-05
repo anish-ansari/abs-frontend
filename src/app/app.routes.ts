e2020c49d96252266d6fedea2b057e40c350a52c
anish-ansari <anishansari489@gmail.com>
2024-05-04 20:36:59 -0700

    Add routes for appointment crud

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { AppointmentListComponent } from './pages/appointment-list/appointment-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NewAppointmentComponent } from './pages/new-appointment/new-appointment.component';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: '', component: HomeComponent }, // for home page
+  { path: 'login', component: LoginComponent }, // for login page
+  { path: 'signup', component: SignupComponent }, // for signup page
+  { path: 'my-appointments', component: AppointmentListComponent }, // when user logs in they're sent to this page
+  { path: 'new-appointment', component: NewAppointmentComponent }, // when user wants to schedule a new appointment
 ];
