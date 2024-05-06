792b81f254aa14dd31d9d73e04cd4667d35595b0
anish-ansari <anishansari489@gmail.com>
2024-05-06 00:38:10 -0700

    Add home component for landing page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent implements OnInit{
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+}
