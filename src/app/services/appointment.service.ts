bb652d823c5967ed8f46469cc3905df5090b3d28
anish-ansari <anishansari489@gmail.com>
2024-05-06 00:40:11 -0700

    Add new appointment service

--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Appointment } from '../models/appointment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  api: string = 'http://localhost:8085/abs/api/v1/appointments';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  // CREATE
+  saveAppointment(appointment: Appointment): Observable<string> {
+    return this.http.post<string>(this.api, appointment, { responseType: 'text' as 'json' });
+  }
+
+  // READ
+
+  // UPDATE
+
+  // DELETE
 }
