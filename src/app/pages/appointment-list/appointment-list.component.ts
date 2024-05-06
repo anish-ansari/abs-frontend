72ffcd229c7ecf1f31edb87c023c925c29d708a2
anish-ansari <anishansari489@gmail.com>
2024-05-06 00:43:34 -0700

    Add comparison value for html file

--- src/app/pages/appointment-list/appointment-list.component.ts
+++ src/app/pages/appointment-list/appointment-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.scss'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit{
+  constructor() {}
+  fullName: string = localStorage.getItem('fullName');
+  valueOfIsAdmin: string = localStorage.getItem('isAdmin');
+  isAdmin = (this.valueOfIsAdmin === 'true')
 
+
+  ngOnInit(): void {
+  }
 }
