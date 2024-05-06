17f033b50cea403a7b054e602568725da2c1215b
anish-ansari <anishansari489@gmail.com>
2024-05-06 00:41:06 -0700

    Change routing after signup

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -37,7 +37,7 @@ export class SignupComponent implements OnInit{
     this.userService.signUp(user).subscribe((result: string) => {
       if (result === 'success') {
         alert('Sign up success, please login now');
-        this.router.navigate(['']);
+        this.router.navigate(['/login']);
       }
       else if (result === 'User already exists')
         alert(result);
